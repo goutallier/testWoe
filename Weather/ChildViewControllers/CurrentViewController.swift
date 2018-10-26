04d7926e292253ba04f30cd3b794b4fec5ffe471
Geoffrey Goutallier <geoffrey@goutallier.net>
2018-10-26 22:56:05 +0200

    add currentWeather var and reload() to CurrentViewController

--- Weather/ChildViewControllers/CurrentViewController.swift
+++ Weather/ChildViewControllers/CurrentViewController.swift
@@ -13,12 +13,35 @@ class CurrentViewController: UIViewController {
     @IBOutlet weak var cityNameLbl: UILabel!
     @IBOutlet weak var summaryLbl: UILabel!
     @IBOutlet weak var temperatureLbl: UILabel!
+    
+    var currentWeather: CurrentWeatherData?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    func reload() {
+        guard let cityName = currentWeather?.name else {
+            return
+        }
+        
+        cityNameLbl.text = cityName
+        
+        guard let summary = currentWeather?.weather.first?.description else {
+            return
+        }
+        
+        summaryLbl.text = summary
+        
+        guard let temp = currentWeather?.main.temp else {
+            return
+        }
+        
+        temperatureLbl.text = String(temp)
+    }
+    
     
 
     /*
