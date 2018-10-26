20c47792ff667354430962ee95907fa3fa94f27a
Geoffrey Goutallier <geoffrey@goutallier.net>
2018-10-26 23:22:29 +0200

    add ref to childviewcontrollers, and logic to handle downloads

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,13 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // these vars will hold the reference to the respective childViewControllers
+    fileprivate var currentViewController: CurrentViewController?
+    fileprivate var forecastsViewController: ForecastsViewController?
+    fileprivate var statsViewController: StatsViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.title = "Weather"
+        
+        // load current weather data
+        let cwn = CurrentWeatherNetworking()
+        cwn.downloadCurrentWeather(completionHandler: didDownloadCurrentData(current:message:))
+        
+        // load forecast data
+        
     }
 
+    func didDownloadCurrentData(current : CurrentWeatherData?, message: String?) {
+        guard let current = current else {
+            return
+        }
+        // populate views current and stats
+        currentViewController?.currentWeather = current
+        currentViewController?.reload()
+        
+        // remove main activity indicator 
+    }
 
+    // prepare(for segue, sender) is used here to retrieve the references of childViewControllers
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination
+        
+        if let currentController = destination as? CurrentViewController {
+            currentViewController = currentController
+        }
+        
+        if let forecastController = destination as? ForecastsViewController {
+            forecastsViewController = forecastController
+        }
+        
+        if let statsController = destination as? StatsViewController {
+            statsViewController = statsController
+        }
+    }
 }
+
 
