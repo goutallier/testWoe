5a63b91b4380ba618a19b749a75328f5da5c3c55
Geoffrey Goutallier <geoffrey@goutallier.net>
2018-10-26 22:51:49 +0200

    add completionHandler to downloadCurrentWeather() and tidy up CurrentWeatherNetworking

--- Weather/Networking/CurrentWeatherNetworking.swift
+++ Weather/Networking/CurrentWeatherNetworking.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class CurrentWeatherNetworking: NSObject {
 
-    func downloadCurrentWeather() {
-        let url = URL(string:"https://samples.openweathermap.org/data/2.5/forecast/daily?id=524901&appid=b1b15e88fa797225412429c1c50c122a1")!
+    func downloadCurrentWeather(completionHandler: @escaping (CurrentWeatherData?, String) -> Void) {
+        let url = URL(string:"https://samples.openweathermap.org/data/2.5/weather?q=Lyon&appid=b1b15e88fa797225412429c1c50c122a1")!
         let task = URLSession.shared.dataTask(with: url) {data, response, error in
             if let error = error {
                 print(error)
@@ -30,39 +30,19 @@ class CurrentWeatherNetworking: NSObject {
             }
             
             DispatchQueue.main.async {
-                self.decodeJSON(with: data)
+                let currentWeather = self.decodeJSON(with: data)
+                completionHandler(currentWeather, "")
             }
         }
         task.resume()
     }
     
-    func decodeJSON(with data: Data) {
+    func decodeJSON(with data: Data) -> CurrentWeatherData? {
         let decoder = JSONDecoder()
-        guard let forecastData = try? decoder.decode(CurrentForecast.self, from: data) else {
+        guard let forecastData = try? decoder.decode(CurrentWeatherData.self, from: data) else {
             print("error: Could not decode JSON into currentForecast")
-            return
+            return nil
         }
-        
-        forecastData.list.count
-        // convert timestamp
-        let forecast = forecastData.list.first as! Forecast
-        let dt = Date(timeIntervalSince1970: forecast.dt)
-        
-        // convert timestamp to weekday
-        let formatter = DateFormatter()
-        formatter.locale = Locale.current
-        formatter.dateFormat = "EEEE"
-        let day = formatter.string(from: dt)
-        
-        // retrieve min and max temp
-        let temp = forecast.temp
-        print(convertToCelsius(tempKelvin: temp.max))
-        print(convertToCelsius(tempKelvin: temp.min))
-    }
-    
-    func convertToCelsius(tempKelvin: Double) -> String {
-        let tempKelvin = Measurement.init(value: tempKelvin, unit: UnitTemperature.kelvin)
-        let tempCelsius = tempKelvin.converted(to: UnitTemperature.celsius)
-        return String("\(Int(round(tempCelsius.value)))º")
+        return forecastData
     }
 }
