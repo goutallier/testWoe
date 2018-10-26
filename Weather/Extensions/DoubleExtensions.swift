685da41738bbe4cf6387c9bf6d75f2f0abb060c0
Geoffrey Goutallier <geoffrey@goutallier.net>
2018-10-26 23:39:44 +0200

    add convertToHour() double's extension

--- Weather/Extensions/DoubleExtensions.swift
+++ Weather/Extensions/DoubleExtensions.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 extension Double {
+    
     func convertToCelsius() -> String {
         // convert tº from K to Cº
         let tempKelvin = Measurement.init(value: self, unit: UnitTemperature.kelvin)
@@ -16,5 +17,23 @@ extension Double {
         return String("\(Int(tempCelsius.value.rounded()))º")
     }
     
+    enum hourFormat {
+        case military
+        case american
+    }
     
+    func convertToHour(format: hourFormat) -> String {
+        let dt = Date(timeIntervalSince1970: self)
+        let formatter = DateFormatter()
+        
+        switch format {
+        case .american:
+            formatter.dateFormat = "h:mm a"
+        default:
+            // in military time:
+            formatter.dateFormat = "HH:mm"
+        }
+        
+        return formatter.string(from: dt)
+    }
 }
