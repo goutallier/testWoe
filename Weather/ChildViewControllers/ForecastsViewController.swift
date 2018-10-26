c4cc28a53129d24a20546d9aa6df1f70ab1ea2ae
Geoffrey Goutallier <geoffrey@goutallier.net>
2018-10-26 13:31:29 +0200

    set ForecastDataProvider as Datasource / Delegate of ForecastViewController's tableview

--- Weather/ChildViewControllers/ForecastsViewController.swift
+++ Weather/ChildViewControllers/ForecastsViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ForecastsViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var dataProvider: (UITableViewDataSource & UITableViewDelegate)!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = dataProvider
+        tableView.delegate = dataProvider
 
         // Do any additional setup after loading the view.
     }
