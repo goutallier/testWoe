cdd05c719696aa22bb052d64f7c1ba660647633f
Geoffrey Goutallier <geoffrey@goutallier.net>
2018-10-26 13:27:10 +0200

    create ForecastDataProvider.swift as Delegate / Datasource

---
+++ Weather/DataProviders/ForecastDataProvider.swift
@@ -0,0 +1,27 @@
+//
+//  ForecastDataProvider.swift
+//  Weather
+//
+//  Created by Geoffrey Goutallier on 26/10/2018.
+//  Copyright © 2018 Geoffrey Goutallier. All rights reserved.
+//
+
+import UIKit
+
+class ForecastDataProvider: NSObject, UITableViewDataSource, UITableViewDelegate {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Totototottotttoottoottotto"
+        cell.backgroundColor = .red
+        return UITableViewCell()
+    }
+}
