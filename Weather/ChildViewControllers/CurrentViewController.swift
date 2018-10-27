38eea152d0a6ccdd91afffe9c65db925180fbce0
Geoffrey Goutallier <geoffrey@goutallier.net>
2018-10-27 09:16:34 +0200

    remove comments

--- Weather/ChildViewControllers/CurrentViewController.swift
+++ Weather/ChildViewControllers/CurrentViewController.swift
@@ -39,19 +39,6 @@ class CurrentViewController: UIViewController {
             return
         }
         
-        temperatureLbl.text = temp.convertToCelsius()
+        temperatureLbl.text = temp.convertToCelsius(.withUnit)
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
